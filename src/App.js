66a0dc814d5ae2c049fc66af22594a3fd7cdf96a
Sonikesh <91758635+SonikeshKumar14@users.noreply.github.com>
2023-03-10 21:33:14 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import Slider from "./components/Slider";
+// link --> https://640b5446b2f6d12eca09d949--gorgeous-daifuku-404376.netlify.app/
+function App() {
+  return <Slider />;
+}
+
+export default App;
